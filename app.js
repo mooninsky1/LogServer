52f0d978bc81034195e80fe24c6ac0bcfe7edce2
mooninsky1 <mooninsky1@users.noreply.github.com>
2019-12-20 16:05:43 +0800

    公告跟服务器列表获取
    公告跟服务器列表获取

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ var app = express();
 var  fs = require('fs');
 var  urlencode = require('urlencode');
 var  log_process = require('./process.js');
+var db = require('./db.js');
 var multer = require('multer');//Multer是一个用于处理multipart / form-data的node.js中间件，主要用于上传文件。它构建在busboy基础上以提高效率。点击 这里 阅读更多关于multer包。
 
+//这3行代码很关键，要不然post上来的数据取不到body
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());//数据JSON类型
+app.use(bodyParser.urlencoded({ extended: false }));//解析post请求数据
 
 //会打开目录html下的index.html 静态,
 app.use(express.static(__dirname + '/html'));
 
-app.listen(conf.GM_SERVER_PORT.app_port);
+
 app.get('/', function(req, res) {
     let log=urlencode.decode(req.url);
     });
+
+    //处理日志
 app.get('/log', function(req, res) {
     let log=urlencode.decode(req.url);
     log_process.log_process(log);
@@ -46,4 +53,122 @@ app.post("/api/Upload", function (req, res) {
     });
 });
 
+app.get('/action', function (req, res) {
+    /*res.writeHead(200,{'Content-Type':'text/html'})
+    fs.readFile('./app.js','utf-8',function(err,data){
+        if(err)
+        {
+            console.error(err);
+            throw err ;
+        }
+        else{
+            return  res.end(data);
+        }
+       
+    });
+    return;
+    */
+    if(req.query.opt == "getnotice")
+    {
+        //res.writeHead(200,{'Content-Type':'text/html'})
+        fs.readFile('./Images/notice.txt','utf-8',function(err,data){
+            if(err)
+            {
+                console.error(err);
+                throw err ;
+            }
+            else{
+                return  res.end(data);
+            }
+           
+        });
+        return;
+       // res.end("getnotice");
+    }
+    else if (req.query.opt == "getlist") {
+        var sql = "SELECT * from server";
+        //console.log(sql);
+        db.querySql(sql, "", function (err, result) {//查询所有users表的数据
+            if (err) {
+                console.log("db err");
+            }
+            else {
+                //console.log(result);
+                var list=[];
+                var def = 0;
+                result.recordset.forEach(element => {
+                    if(element.open > 0){
+                        list.push({stat:element.stat, name:element.name, id:element.id });
+                        if(element.flag>=1){
+                            def = element.id;
+                        }
+                    }
+                    
+                });
+                //for (var i = 0; i < result.recordsets.length; i++) {
+                 //   list.push({stat:result.recordsets[i].stat, name:result.recordsets[i]., id: 1 });
+               // }
+                var serverlist={list:list, default:def};
+               // console.log(JSON.stringify(serverlist));
+                res.end(JSON.stringify(serverlist));
+                return;
+
+            }
+        });
+        //fs.readFile('./Images/server.txt', 'utf-8', function (err, data) {
+         //   if (err) {
+        //        throw err;
+        //    }
+        //    res.end(data);
+        //});
+        return;
+    }
+});
+app.post('/notice', function(req,res){
+    console.log('notice post')
+    console.log(req.body);
+    res.writeHead(200,{'Content-Type':'text/html'})
+    var data= { title: req.body.title, content:req.body.context };
+    data = JSON.stringify(data);
+    fs.writeFile('./Images/notice.txt',data,function(err){
+        if(err)
+        {
+            res.end("commit fail");
+        }
+        else{
+            res.end("commit ok");
+        }
+    });
+})
+var params = {
+    "id":5,
+    "name":"白眉鹰王"
+}
+//写入json文件选项
+function writeJson(params){
+    //现将json文件读出来
+    fs.readFile('./mock/person.json',function(err,data){
+        if(err){
+            return console.error(err);
+        }
+        var person = data.toString();//将二进制的数据转换为字符串
+        person = JSON.parse(person);//将字符串转换为json对象
+        person.data.push(params);//将传来的对象push进数组对象中
+        person.total = person.data.length;//定义一下总条数，为以后的分页打基础
+        console.log(person.data);
+        var str = JSON.stringify(person);//因为nodejs的写入文件只认识字符串或者二进制数，所以把json对象转换成字符串重新写入json文件中
+        fs.writeFile('./mock/person.json',str,function(err){
+            if(err){
+                console.error(err);
+            }
+            console.log('----------新增成功-------------');
+        })
+    })
+}
+//writeJson(params)//执行一下;
+
+
+
+
+app.listen(conf.GM_SERVER_PORT.app_port);
 console.log("localhost:"+conf.GM_SERVER_PORT.app_port);
--- config.js
+++ config.js
@@ -1,4 +1,19 @@
-
+//GM管理工具，账号数据库
+var GM_Account_DB = {
+    user: 'sa',
+    password: '123456',
+    server: '127.0.0.1',
+    database: 'nodetest',
+    port: 1433,
+    options: {
+    encrypt: true // Use this if you're on Windows Azure
+    },
+    pool: {
+        min: 0,
+        max: 10,
+        idleTimeoutMillis: 3000
+    }
+};
 //游戏服务器数据库
 var GM_SERVER_DB = {
     user: 'sa',
@@ -19,15 +34,16 @@ var GM_SERVER_DB = {
 //log日志数据库
 var GM_LOGSERVER_DB = {
     port : 6379,
-    server: '120.76.235.228',
+    server: '127.0.0.1',
     password:'lhf123456'
 };
 //GM服务器监听端口
 var GM_SERVER_PORT = {
-    app_port : 6000,            //浏览器端口
+    app_port : 7656,            //浏览器端口  google 限制某些端口
     socket_io_port : 8081,      //浏览器跟后端node.js 使用socket.io通信端口
 };
 
+module.exports.GM_Account_DB = GM_Account_DB;
 module.exports.GM_SERVER_DB = GM_SERVER_DB;
 module.exports.GM_LOGSERVER_DB = GM_LOGSERVER_DB;
 module.exports.GM_SERVER_PORT = GM_SERVER_PORT;
